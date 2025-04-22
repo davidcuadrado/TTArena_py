76507ae381bc61202fc083f0bd037abd6214f33c
David Cuadrado <dav.cuadrado@gmail.com>
2025-04-22 23:51:58 +0200

    refactor: fixed project structure issues with duplicated package name

--- ttarena_backend/urls.py
+++ ttarena_backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ttarena_backend.core import views
+from core import views
 
 urlpatterns = [
     path('', views.home, name="home"),
