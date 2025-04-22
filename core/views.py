35a1df5c8c3bc12a71c6c6f54daa2927ad7ed94a
David Cuadrado <dav.cuadrado@gmail.com>
2025-04-23 00:12:44 +0200

    feature: implemented home and about urls

--- core/views.py
+++ core/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = """
+<h1> TT ARENA </h1>
+<ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="about/">About</a></li>
+</ul>    
+"""
+
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>TTArena Home</h1><h2>Landing page<h2>")
+    return HttpResponse(html_base + """ 
+        <h2>Landing page<h2> 
+        <p>Welcome to TT Arena</p>
+    """)
 
+def about(request):
+    return HttpResponse(html_base + """ 
+            <h2>About TTArena<h2>
+            <p>An online Tabletop Arena game based on WoW</p>
+        """)
