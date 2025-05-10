ba2a0bab8b66df98dbe976500d09834017fdc4db
davidcuadrado <dav.cuadrado@gmail.com>
2025-05-10 20:51:52 +0200

    feature: added link atribute to overview models

--- overview/models.py
+++ overview/models.py
@@ -5,8 +5,10 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to="projects")
+    link = models.URLField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name="creation date")
     updated = models.DateTimeField(auto_now=True, verbose_name="last update date")
+
 
     class Meta:
         verbose_name = "project"
