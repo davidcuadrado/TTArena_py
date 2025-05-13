488dffae1073dc2130d26b332ab2b4af8b2cf5ab
dav.cuadrado <dav.cuadrado@gmail.com>
2025-05-13 19:53:18 +0000

    feature: added password_change urls

--- ttarena_backend/urls.py
+++ ttarena_backend/urls.py
@@ -35,6 +35,10 @@ urlpatterns = [
     path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
     path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'), name='password_change'), # Add this line if not using include('django.contrib.auth.urls')
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'), # Add this line if not using include('django.contrib.auth.urls')
+
+
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
