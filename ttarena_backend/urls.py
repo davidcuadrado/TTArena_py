b37862a9a0d98f42c941c7e9ac8f2248685a6237
dav.cuadrado <dav.cuadrado@gmail.com>
2025-05-13 19:48:16 +0000

    fix: removed unused path

--- ttarena_backend/urls.py
+++ ttarena_backend/urls.py
@@ -35,9 +35,6 @@ urlpatterns = [
     path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
     path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 
-    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'), name='password_change'),
-    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
-
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
