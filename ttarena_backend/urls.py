94fcafe8143f7afa4588f387c283e019524d3354
K1-PC2 <dav.cuadrado@gmail.com>
2025-05-18 15:13:16 +0200

    feature: service admin update

--- ttarena_backend/urls.py
+++ ttarena_backend/urls.py
@@ -35,8 +35,8 @@ urlpatterns = [
     path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
     path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 
-    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'), name='password_change'), # Add this line if not using include('django.contrib.auth.urls')
-    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'), # Add this line if not using include('django.contrib.auth.urls')
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'), name='password_change'),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
 
 
     path('', include('core.urls')),
