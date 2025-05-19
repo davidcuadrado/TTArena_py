baf5aaa79e4037d490f03119f6b0364723874a7c
K1-PC2 <dav.cuadrado@gmail.com>
2025-05-19 20:38:35 +0200

    fix: added proper dinamic urls to footer pages

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.page, name='pages'),
-    path('<int:page_id>/', views.page, name='page'),
+    path('<int:page_id>/<slug:page_slug>/', views.page, name='page'),
 ]
